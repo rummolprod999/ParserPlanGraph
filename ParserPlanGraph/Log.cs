a46a86624d15eb17e9fe4206f6da80b41599019b
rummolprod999 <rummolprod999@gmail.com>
2017-06-21 14:28:11 +0300

    update

---
+++ ParserPlanGraph/Log.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+
+namespace ParserPlanGraph
+{
+    public class Log
+    {
+        public static void Logger(params object[] parametrs)
+        {
+            string s = "";
+            s += DateTime.Now.ToString(CultureInfo.InvariantCulture);
+            for (int i = 0; i < parametrs.Length; i++)
+            {
+                s = $"{s} {parametrs[i]}";
+            }
+
+            using (StreamWriter sw = new StreamWriter(Program.FileLog, true, System.Text.Encoding.Default))
+            {
+                sw.WriteLine(s);
+            }
+        }
+    }
+}
