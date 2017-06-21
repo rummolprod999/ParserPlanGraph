75b70e14a3edaf15550175dd558eaceb2cd6196e
rummolprod999 <rummolprod999@gmail.com>
2017-06-21 14:11:57 +0300

    Initial commit

---
+++ ParserPlanGraph/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ParserPlanGraph
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
